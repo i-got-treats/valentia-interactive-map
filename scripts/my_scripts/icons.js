a335b82fe0c2c313bcd76be83b8f2a1622c3da8d
Oliwia Daszczynska <do_wyrzucenia@outlook.de>
2022-12-08 19:13:11 +0100

    icons in separate file

--- scripts/my_scripts/icons.js
+++ scripts/my_scripts/icons.js
@@ -4,38 +4,152 @@ var p_anchor = [0, -26];
 
 var doticon = L.icon({
     iconUrl: 'images/icons/unused/dot.png',
-    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
-});
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
 
-/* BUILDINGS*/
+
+/* BUILDINGS */
+var castle_i = L.icon({
+    iconUrl: 'images/icons/buildings/castle.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var cathedral_i = L.icon({
+    iconUrl: 'images/icons/buildings/cathedral.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
 var colig_i = L.icon({
     iconUrl: 'images/icons/buildings/colig.png',
-    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
-})
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var crypts_i = L.icon({
+    iconUrl: 'images/icons/buildings/crypts.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var evil_tower_i = L.icon({
+    iconUrl: 'images/icons/buildings/evil_tower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var fort_i = L.icon({
+    iconUrl: 'images/icons/buildings/fort.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var keep_i = L.icon({
+    iconUrl: 'images/icons/buildings/keep.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
 
 var lighthouse_i = L.icon({
     iconUrl: 'images/icons/buildings/lighthouse.png',
-    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
-});
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
 
-/* LANDMARKS*/
+var mill_i = L.icon({
+    iconUrl: 'images/icons/buildings/mill.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
 
-/*LOCATIONS*/
+var stronghold_i = L.icon({
+    iconUrl: 'images/icons/buildings/stronghold.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var tavern_i = L.icon({
+    iconUrl: 'images/icons/buildings/tavern.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var temple_i = L.icon({
+    iconUrl: 'images/icons/buildings/temple.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var tower_i = L.icon({
+    iconUrl: 'images/icons/buildings/tower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var watchtower_i = L.icon({
+    iconUrl: 'images/icons/buildings/watchtower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var windmill_i = L.icon({
+    iconUrl: 'images/icons/buildings/windmill.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+/* BUILDINGS end */
+
+
+/* LANDMARKS */
+var castle_i = L.icon({
+    iconUrl: 'images/icons/buildings/castle.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var cathedral_i = L.icon({
+    iconUrl: 'images/icons/buildings/cathedral.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var colig_i = L.icon({
+    iconUrl: 'images/icons/buildings/colig.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var crypts_i = L.icon({
+    iconUrl: 'images/icons/buildings/crypts.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var evil_tower_i = L.icon({
+    iconUrl: 'images/icons/buildings/evil_tower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var fort_i = L.icon({
+    iconUrl: 'images/icons/buildings/fort.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var keep_i = L.icon({
+    iconUrl: 'images/icons/buildings/keep.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var lighthouse_i = L.icon({
+    iconUrl: 'images/icons/buildings/lighthouse.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var mill_i = L.icon({
+    iconUrl: 'images/icons/buildings/mill.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var stronghold_i = L.icon({
+    iconUrl: 'images/icons/buildings/stronghold.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var tavern_i = L.icon({
+    iconUrl: 'images/icons/buildings/tavern.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var temple_i = L.icon({
+    iconUrl: 'images/icons/buildings/temple.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var tower_i = L.icon({
+    iconUrl: 'images/icons/buildings/tower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var watchtower_i = L.icon({
+    iconUrl: 'images/icons/buildings/watchtower.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+
+var windmill_i = L.icon({
+    iconUrl: 'images/icons/buildings/windmill.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+/* LANDMARKS end*/
+
+
+/* LOCATIONS */
+
+/* LOCATIONS end */
+
 
 /* SETTLEMENTS */
 var camp_i = L.icon({
     iconUrl: 'images/icons/settlements/camp.png',
-    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
-})
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor})
 
 var city_i = L.icon({
     iconUrl: 'images/icons/settlements/city.png',
     iconSize: [85, 60],
     iconAnchor: [42, 30],
-    popupAnchor: [0, -36]
-});
+    popupAnchor: [0, -36]});
 
 var town_i = L.icon({
     iconUrl: 'images/icons/settlements/town.png',
-    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
-});
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor});
+    /* SETTLEMENTS end */
