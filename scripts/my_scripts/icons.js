646c6c80336f17af50f421a76d85dd1b57aaffd2
Oliwia Daszczynska <do_wyrzucenia@outlook.de>
2022-12-08 17:07:19 +0100

    test if splitting into folders works

---
+++ scripts/my_scripts/icons.js
@@ -0,0 +1,41 @@
+var i_size = [46, 40];
+var i_anchor = [23, 20];
+var p_anchor = [0, -26];
+
+var doticon = L.icon({
+    iconUrl: 'images/icons/unused/dot.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
+});
+
+/* BUILDINGS*/
+var colig_i = L.icon({
+    iconUrl: 'images/icons/buildings/colig.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
+})
+
+var lighthouse_i = L.icon({
+    iconUrl: 'images/icons/buildings/lighthouse.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
+});
+
+/* LANDMARKS*/
+
+/*LOCATIONS*/
+
+/* SETTLEMENTS */
+var camp_i = L.icon({
+    iconUrl: 'images/icons/settlements/camp.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
+})
+
+var city_i = L.icon({
+    iconUrl: 'images/icons/settlements/city.png',
+    iconSize: [85, 60],
+    iconAnchor: [42, 30],
+    popupAnchor: [0, -36]
+});
+
+var town_i = L.icon({
+    iconUrl: 'images/icons/settlements/town.png',
+    iconSize: i_size, iconAnchor: i_anchor, popupAnchor: p_anchor
+});
